98d30bc64bf3d71a993089562b7bbd9414a335cc
Khamraj Rohit <khamraj@luminousvector.com>
2018-07-14 00:30:44 -0400

    Misc

--- Meteora/Program.cs
+++ Meteora/Program.cs
@@ -8,9 +8,21 @@ using System.Windows.Forms;
 using System.Threading;
 using Meteora.View;
 using Vulkan;
+using System.Runtime.InteropServices;
+using System.IO;
 
 class Program
 {
+
+	[DllImport("kernel32.dll", EntryPoint = "GetStdHandle", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+	public static extern IntPtr GetStdHandle(int nStdHandle);
+
+	[DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+	public static extern int AllocConsole();
+
+	private const int STD_OUTPUT_HANDLE = -11;
+	private const int MY_CODE_PAGE = 437;
+
 	[STAThread]
 	static void Main(string[] args)
 	{
@@ -22,13 +34,18 @@ class Program
 			Application.Run(game);
 			game.Dispose();
 		});
+		Console.Write("Creating window... ");
 		thread.Start();
 		while (true)
 		{
 			if (game != null)
 				break;
 		}
+		Console.WriteLine("Done!");
+		Console.Write("Initializing... ");
 		game.Init();
+		Console.WriteLine("Done!");
+		Console.WriteLine("Running Main Loop... ");
 		game.DoMainLoop();
 		thread.Join();
 	}
